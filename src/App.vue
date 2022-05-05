de969937da22312ea46013896309b672efa27374
Alex <atomaspe29@alumnes.ub.edu>
2022-05-05 20:43:18 +0200

    Firts things.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+        app
+        color="primary"
+        dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+        />
+      </div>
+
+      <div :style="{width: '500px'}">
+        <v-text-field
+            v-model="message"
+            hide-details
+            prepend-icon="mdi-magnify"
+            append-icon = "mdi-filter"
+            clear-icon="mdi-close-circle"
+            clearable
+            label="Introduce tu búsqueda..."
+            type="text"
+            single-line
+            dark
+            @click:clear="clearMessage"
+        ></v-text-field>
+      </div>
+
+    </v-app-bar>
+
+    <v-card>
+      <v-navigation-drawer
+          color="blue darken-2"
+          dark
+          v-model="drawer"
+          :mini-variant.sync="mini"
+          permanent
+          app right
+      >
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-title>CatLover22</v-list-item-title>
+
+          <v-btn
+              icon
+              @click.stop="mini = !mini"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              link
+          >
+            <v-list-item-icon>
+              <v-icon color="black">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+
+    <v-main>
+      <v-btn
+          :left="true"
+          :absolute="true"
+          class="position-absolute bottom-0 start-0"
+          bottom
+          elevation="2"
+          icon = "mdi-human"
+          outlined
+      ></v-btn>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  methods: {
+    clearMessage () {
+      this.message = ''
+    },
+  },
+
+  data () {
+    return {
+      drawer: true,
+      items: [
+        { title: 'Home', icon: 'mdi-home-city' },
+        { title: 'My Account', icon: 'mdi-account' },
+        { title: 'Users', icon: 'mdi-account-group-outline' },
+      ],
+      mini: true,
+    }
+  },
+};
+</script>
